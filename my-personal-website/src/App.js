a9bdfead1a524b918ce0e0d6b69948ff5f8decfe
zheruixS2023 <xiezherui99@gmail.com>
2023-09-07 03:13:34 -0700

    init commit

---
+++ my-personal-website/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <div className="jumbotron">
+        <h1 className="display-4">Welcome to [Your Name]'s Website!</h1>
+        <p className="lead">This is a simple React-based personal website with Bootstrap styling hosted on GitHub Pages.</p>
+        <hr className="my-4" />
+        <p>You can add more about your skills, projects, and anything else you'd like to showcase here.</p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
